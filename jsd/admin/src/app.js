eeb2f5bf4e6b911bf098ee4f15d7b79b4ec5d3af
jsdno0 <csdnum0@gmail.com>
2022-01-19 23:09:40 +0900

    backend with rabbitmq

---
+++ jsd/admin/src/app.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var cors = require("cors");
+var app = express();
+app.use(cors({
+    origin: ['http://localhost:3000', 'http://localhost:8080', 'http://localhost:4200']
+}));
+app.use(express.json());
+console.log('Listening to port: 8000');
+app.listen(8000);
