21ff97872400410ba5640a649dfbb2711d75f2a1
jsdno0 <csdnum0@gmail.com>
2022-01-22 07:17:11 +0900

    commit

--- jsd/admin/src/app.ts
+++ jsd/admin/src/app.ts
@@ -1,14 +1,22 @@
 import * as express from 'express';
 import * as cors from 'cors';
+import {createConnection} from 'typeorm'
 
-const app = express();
+createConnection().then(db => {
+    
+    const app = express();
+    
+    app.use(cors({
+        origin: ['http://localhost:3000','http://localhost:8080','http://localhost:4200']
+    }));
+    
+    app.use(express.json());
+    
+    console.log('Listening to port: 8000');
+    
+    app.listen(8000);
 
-app.use(cors({
-    origin: ['http://localhost:3000','http://localhost:8080','http://localhost:4200']
-}));
-
-app.use(express.json());
-
-console.log('Listening to port: 8000');
-
-app.listen(8000);
+}).catch(err=>{
+    console.log(err);
+    throw err;
+})
---
+++ jsd/admin/src/entity/Product.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Product {
+    
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    image: String;
+
+    @Column({default:0})
+    likes: number;
+}
