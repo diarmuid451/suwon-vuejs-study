c4ff3cb5631a1cd45543a307363b0fde1c325dbe
byeongjeon jeon <pensieve92@gmail.com>
2022-01-29 20:35:49 +0900

    edit mainPage

--- jbh/vue-til-clone/src/App.vue
+++ jbh/vue-til-clone/src/App.vue
@@ -1,52 +1,63 @@
 <template>
 	<v-app>
 		<v-app-bar app color="primary" dark>
-			<div class="d-flex align-center">
-				<v-img
-					alt="Vuetify Logo"
-					class="shrink mr-2"
-					contain
-					src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-					transition="scale-transition"
-					width="40"
-				/>
-
-				<v-img
-					alt="Vuetify Name"
-					class="shrink mt-1 hidden-sm-and-down"
-					contain
-					min-width="100"
-					src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-					width="100"
-				/>
-			</div>
+			<v-toolbar-title>저기요</v-toolbar-title>
 			<v-spacer></v-spacer>
-
-			<v-btn
-				href="https://github.com/vuetifyjs/vuetify/releases/latest"
-				target="_blank"
-				text
-			>
-				<span class="mr-2">Latest Release</span>
-				<v-icon>mdi-open-in-new</v-icon>
-			</v-btn>
+			<v-btn elevation="2"> 로그인 </v-btn>
 		</v-app-bar>
-
-		<v-main>
-			<HelloWorld />
-		</v-main>
+		<v-parallax
+			height="300"
+			dark
+			src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+		>
+			<v-row align="center" justify="center">
+				<v-col class="text-center" cols="5">
+					<v-form>
+						<v-container>
+							<v-row>
+								<v-col cols="12">
+									<v-text-field
+										v-model="message"
+										outlined
+										clearable
+										label="건물명, 도로명, 지명으로 검색하세요"
+										type="text"
+									>
+										<template v-slot:prepend>
+											<v-btn>
+												<v-icon align="center"> mdi-target </v-icon>
+											</v-btn>
+										</template>
+										<template v-slot:append>
+											<v-fade-transition leave-absolute>
+												<v-progress-circular
+													v-if="loading"
+													size="24"
+													color="info"
+													indeterminate
+												></v-progress-circular>
+											</v-fade-transition>
+										</template>
+										<template v-slot:append-outer>
+											<v-btn> 검색 </v-btn>
+										</template>
+									</v-text-field>
+								</v-col>
+							</v-row>
+						</v-container>
+					</v-form>
+					<!--          버튼-->
+				</v-col>
+			</v-row>
+		</v-parallax>
 	</v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
 	name: 'App',
 
-	components: {
-		HelloWorld,
-	},
+	components: {},
 
 	data: () => ({
 		//
