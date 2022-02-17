ce8b6b7497ce20d2f155058c1689608a8c0dc9d7
jsdno0 <csdnum0@gmail.com>
2022-02-17 22:55:35 +0900

    front work

--- jsd/front/src/pages/admin/Products.vue
+++ jsd/front/src/pages/admin/Products.vue
@@ -4,19 +4,19 @@
           <thead>
             <tr>
               <th scope="col">#</th>
-              <th scope="col">Header</th>
-              <th scope="col">Header</th>
-              <th scope="col">Header</th>
-              <th scope="col">Header</th>
+              <th scope="col">Image</th>
+              <th scope="col">Title</th>
+              <th scope="col">Likes</th>
+              <th scope="col">Action</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1,001</td>
-              <td>random</td>
-              <td>data</td>
-              <td>placeholder</td>
-              <td>text</td>
+            <tr v-for="product in products" :key="product.id">
+              <td>{{product.id}}</td>
+              <td><img :src="product.image" height="50" /></td>
+              <td>{{product.title}}</td>
+              <td>{{product.likes}}</td>
+              <td></td>
             </tr>
           </tbody>
         </table>
@@ -24,7 +24,20 @@
 </template>
 
 <script>
+import { ref, onMounted } from 'vue'
     export default {
-        name: "Products"
+        name: "Products",
+        setup() {
+          const products = ref([]); //ref: products가 변하면 화면단이 rendering 된다
+          onMounted( async () => {
+            const response = await fetch('http://localhost:8000/api/products');
+            const data = await response.json();
+
+            console.log(data);
+          })
+          return{
+            products
+          }
+        }
     }
 </script>
