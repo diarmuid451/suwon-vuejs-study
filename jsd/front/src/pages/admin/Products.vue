d183ffb689ccb0e313b29e8b632a41c2c3ae03a5
JS <csdnum0@gmail.com>
2022-03-03 15:27:26 +0900

    Update Products.vue

--- jsd/front/src/pages/admin/Products.vue
+++ jsd/front/src/pages/admin/Products.vue
@@ -1,4 +1,9 @@
 <template>
+<div class = "pt-3 pb-2 mb-3 border-bottom">
+<div class = "btn-toolbar mb-2 mb-md-0">
+<router-link to = "/admin/products/create" class = "btn btn-sm btn-outline-secondary">Add</a>
+</div>
+</div>
     <div class="table-responsive">
         <table class="table table-striped table-sm">
           <thead>
